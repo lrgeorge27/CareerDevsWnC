52f2631a3a791806eec59aac6756b718bd589926
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-14 03:22:34 +0000

    WnC Version 1

---
+++ WnC_Branch.js
@@ -0,0 +1,19 @@
+//Create an array
+var todos = ["item 1", "item 2", "item 3"];
+
+//Add new items to an array
+todos.push("item 4");
+todos.push("item 5");
+
+//Get a specific item from an array with index#
+todos[0];
+
+//Change an item in an array
+todos[0] = "item 1 updated";
+
+//Delete an item in an array
+todos.splice(0, 1);
+todos.splice(3, 1);
+
+//Display an array
+console.log(todos);
