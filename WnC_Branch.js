54efc4ec5f16ba2e94e6a24d51485439dbabd923
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-20 03:09:40 +0000

    objects version3

--- WnC_Branch.js
+++ WnC_Branch.js
@@ -1,3 +1,4 @@
+//Arrays version1
 //Create an array
 var todos = ["item 1", "item 2", "item 3"];
 
@@ -21,6 +22,7 @@ console.log(todos);
 //Customize functions
 console.log('My Todos', todos);
 
+//Functions Version2
 //Customizing functions with var in ()
 function sayHiTo(person) {
     console.log('hi', person);
@@ -33,7 +35,7 @@ function displayTodos() {
 }
 
 //function to add todos
-function addTodo(todo) {
+function addTodo(todo) { //todo parameter you want to add
     todos.push(todo);
     displayTodos();
 }
@@ -41,7 +43,7 @@ function addTodo(todo) {
 addTodo("hello there");
 
 //function to change todos
-function changeTodo(position, newValue) {
+function changeTodo(position, newValue) { //2 parameters, positon of array item to change, and change to make.
     todos[position] = newValue;
     displayTodos();
 }
@@ -52,9 +54,54 @@ changeTodo(0, 'changed again');
 
 //function to delete todos
 function deleteTodo(position) {
-    todos.splice(position, 1);
+    todos.splice(position, 1); //position of array item you'd like to delete. Number of items to delete at a time. 
     displayTodos();
 }
 
 deleteTodo(0);
 deleteTodo(2);
+
+//Objects version3
+//Creating an object
+var myComputer = {
+    operatingSystem: "windows",
+    screenSize: "12 inches",
+    purchaseYear: 2013
+};
+console.log(myComputer);
+console.log(myComputer.operatingSystem);
+console.log(myComputer.screenSize);
+console.log(myComputer.purchaseYear);
+
+var Lauren = {
+    name: "Lauren",
+    sayName: function() { //function inside object (method), sayName is function name.
+        console.log(this.name); //this refers to the object the function is housed in. 
+    }
+};
+Lauren.sayName();
+
+//Store todos array on an object & create function methods
+var todoList = {
+    todos: ["item 1", "item 2", "item 3"],
+    displayTodos: function() { //displayTodos method
+        console.log("My Todos:", this.todos);
+    },
+    addTodos: function(todo) { //addTodos method
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) { //changeTodo method
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) { //deleteTodo method
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+};
+
+todoList.displayTodos();
+todoList.addTodos('c9');
+todoList.changeTodo(0, "first");
+todoList.deleteTodo(1);
