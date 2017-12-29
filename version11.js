cb83f9d62cb39ee4d28bc012e92c1e558e22036e
lrgeorge27 <lrgeorge27@gmail.com>
2017-12-29 03:51:50 +0000

    finished version11

---
+++ version11.js
@@ -0,0 +1,151 @@
+//version 8
+//Refactoring - process of restructuring existing code, w/o changing external behavior. Improves nonfuctional attributes of software.
+var todoList = {
+    todos: [],
+    addTodos: function(todoText) { //addTodos method
+        this.todos.push({ //addTodos now creates an object in an array instead of an item.
+            todoText: todoText, //label: parameter 
+            completed: false
+        });
+    },
+    changeTodo: function(position, todoText) { //changeTodo method
+        //this.todos[position] = newValue;
+        this.todos[position].todoText = todoText; //changeTodo now creates new text in the todoText position of the object.
+    },
+    deleteTodo: function(position) { //deleteTodo method
+        this.todos.splice(position, 1);
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position]; //var keeps us from having to type this.todos[position] on both sides of = in next line.
+        todo.completed = !todo.completed; //change value to opposite, false = !false (=true)
+    },
+    toggleAll: function() { //version 6 creating the toggleAll method. If everything is true, make everything false. Otherwise, make everything true.
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        //Get number of completed todos
+        // for (var i = 0; i < totalTodos; i++) {
+        //     if (this.todos[i].completed === true) {
+        //         completedTodos++;
+        //     }
+        // }
+
+        this.todos.forEach(function(todo) {
+            if (todo.completed === true) {
+                completedTodos++;
+            }
+        });
+
+        this.todos.forEach(function(todo) {
+            //Case 1: if everything is true make everything false.
+            if (completedTodos === totalTodos) {
+                todo.completed = false;
+                //Case 2: otherwise, make everything true.
+            }
+            else {
+                todo.completed = true;
+            }
+        });
+    }
+};
+
+//version 8 -refactoring the process of getting elements by id 
+//and adding event listeners to a simplified process with a handler object and onclick attributes.
+var handlers = { //object handles all the methods for different events
+    addTodos: function() {
+        var addTodoTextInput = document.getElementById("addTodoTextInput"); //grabing the text input
+        todoList.addTodos(addTodoTextInput.value); //setting it as an argument to meet the parameter todoText
+        addTodoTextInput.value = ""; //clears the input box on submit
+        view.displayTodos();
+    },
+    changeTodos: function() {
+        var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
+        var changeTodoTextInput = document.getElementById("changeTodoTextInput");
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = "";
+        changeTodoTextInput.value = "";
+        view.displayTodos();
+    },
+    deleteTodo: function(position) {
+        todoList.deleteTodo(position);
+        view.displayTodos();
+    },
+    toggleCompleted: function() {
+        var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput = "";
+        view.displayTodos();
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+        view.displayTodos();
+    }
+};
+
+//version 9 - dynamic list display
+var view = {
+    displayTodos: function() { //creates a dynamic list display.
+        var todosUl = document.querySelector('ul');
+        todosUl.innerHTML = "";
+        // for (var i = 0; i < todoList.todos.length; i++) { //iterate through the todoList array to grab the items.
+        //     var todoLi = document.createElement('li'); //creates a list element.
+        //     var todo = todoList.todos[i]; //create a variable to access each todos item for copletion if/else statement. Saves us from typing todoList.todos[i] repeatedly.
+        //     var todoTextWithCompletion = "";
+
+        //     if (todo.completed === true) {
+        //         todoTextWithCompletion = "(x) " + todo.todoText;
+        //     }
+        //     else {
+        //         todoTextWithCompletion = "( ) " + todo.todoText;
+        //     }
+        //     todoLi.id = i; //create an id and set it equal to the index number
+        //     todoLi.textContent = todoTextWithCompletion; //replaces line below and includes completion info.
+        //     // todoLi.textContent = todoList.todos[i].todoText; //adds the todoText to the list item
+        //     todoLi.appendChild(this.createDeleteButton());
+        //     todosUl.appendChild(todoLi); //places the list element with text into the unordered list.
+        // }
+
+        // this //refers to view object
+        // forEach(callback, this)
+
+        todoList.todos.forEach(function(todo, position) {
+            var todoLi = document.createElement('li'); //creates a list element.
+            var todoTextWithCompletion = "";
+
+            if (todo.completed === true) {
+                todoTextWithCompletion = "(x) " + todo.todoText;
+            }
+            else {
+                todoTextWithCompletion = "( ) " + todo.todoText;
+            }
+
+            todoLi.id = position; //create an id and set it equal to the index number
+            todoLi.textContent = todoTextWithCompletion; //replaces line below and includes completion info.
+            todoLi.appendChild(this.createDeleteButton());
+            todosUl.appendChild(todoLi); //places the list element with text into the unordered list.
+        }, this);
+    },
+    //version 10
+    createDeleteButton: function() {
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete"; //html text in button
+        deleteButton.className = "deleteButton";
+        return deleteButton;
+    },
+    //event delegation
+    setUpEventListeners: function() {
+        var todosUl = document.querySelector("ul");
+        todosUl.addEventListener('click', function(event) { //single event listener, listens for all clicks on the ul.
+            console.log(event.target.parentNode.id); //mouse event array.traget the portion of the element clicked, go into the parentNode to access the li id. Returns li id.
+
+            //get the element that was clicked on. 
+            var elementClicked = event.target;
+
+            //check if elementClicked is a delete button.
+            if (elementClicked.className === 'deleteButton') {
+                handlers.deleteTodo(parseInt(elementClicked.parentNode.id)); //convert string to number
+            }
+        });
+    }
+};
+
+view.setUpEventListeners();
